01d100e60cfae795fda8e642d239b60ff743d4d0
Dzmitry Charnamortsau <d.charnamortsau@gmail.com>
2025-09-24 22:25:27 -0700

    used Math.min

--- workbook1d/MathApplication/src/main/java/com/pluralsight/MathApp.java
+++ workbook1d/MathApplication/src/main/java/com/pluralsight/MathApp.java
@@ -6,5 +6,9 @@ public class MathApp {
         double garySalary = 35.0;
         double highestSalary = Math.max(bobSalary,garySalary);
         System.out.println("The highest salary is " + highestSalary);
+        double carPrice = 19999.99;
+        double truckPrice = 49999.99;
+        double smallestPrice = Math.min(carPrice, truckPrice);
+        System.out.println("The smallest price is " + smallestPrice);
     }
 }
