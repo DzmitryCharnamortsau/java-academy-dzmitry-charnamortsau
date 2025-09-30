5c750856a060ad9068cc39ad740772715c77f7df
Dzmitry Charnamortsau <d.charnamortsau@gmail.com>
2025-09-30 13:49:52 -0700

    created  high score wins app

---
+++ workbook2w/wk2stringsandarrays/FullNameGenerator/src/main/java/com/pluralsight/HighScoreWins.java
@@ -0,0 +1,23 @@
+package com.pluralsight;
+import java.util.Scanner;
+import java.util.regex.Pattern;
+
+public class HighScoreWins {
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter a game score: ");
+        String score = input.nextLine();
+        String [] parts = score.split(("\\|"));
+        String [] teams = parts[0].split((":"));
+        String [] scores = parts[1].split((":"));
+        int homeScore = Integer.parseInt(scores[0]);
+        int visitorScore = Integer.parseInt(scores[1]);
+        if (homeScore > visitorScore){
+            System.out.println("Winner: " + teams[0]);
+        } else if (homeScore < visitorScore) {
+            System.out.println("Winner: " + teams[1]);
+          } else {
+            System.out.println("It's a tie");
+        } input.close();
+    }
+}
