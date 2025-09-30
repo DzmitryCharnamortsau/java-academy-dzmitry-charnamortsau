df4018de8d0f45e1da95b76a7f812297dfcfbdff
Dzmitry Charnamortsau <d.charnamortsau@gmail.com>
2025-09-30 12:15:25 -0700

    created full name generator

--- workbook1d/Demo-Methods/src/main/java/com/pluralsight/Main.java
+++ workbook1d/Demo-Methods/src/main/java/com/pluralsight/Main.java
@@ -4,10 +4,6 @@ package com.pluralsight;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-       Retirement(50);
+        int dayNumber = 3;
     }
-    public static void Retirement(int years) {
-        System.out.println("You have " + (67 - years) + " years left till retirement");
-    }
-
 }
