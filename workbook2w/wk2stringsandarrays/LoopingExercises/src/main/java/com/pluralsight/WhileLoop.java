c528719ca57867be597d7356412f96c5208a6f07
Dzmitry Charnamortsau <d.charnamortsau@gmail.com>
2025-10-01 15:52:31 -0700

    loops exercises

---
+++ workbook2w/wk2stringsandarrays/LoopingExercises/src/main/java/com/pluralsight/WhileLoop.java
@@ -0,0 +1,13 @@
+package com.pluralsight;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class WhileLoop {
+    public static void main(String[] args) {
+        int i = 0;
+        while (i < 5){
+            i++;
+            System.out.println("I love Java");
+        }
+    }
+}
